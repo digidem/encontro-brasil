ae346ee046463ed3a0e55e02eaaf73840bf87887
luandro <luandro@digital-democracy.org>
2024-09-07 09:44:52 -0300

    WIP

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -42,53 +42,77 @@ const Index = () => {
 
   const scheduleTitle = markdownContents['Event Schedule']?.split('\n').find(line => line.startsWith('#'))?.replace(/^#+\s*/, '').trim();
   const scheduleItems = markdownContents['Event Schedule']?.split('\n\n**').slice(1).map(item => `**${item}`);
-  
+
   const eventInfo = extractEventInformation(markdownContents['Event Information'] || '');
-  
+
   return (
     <div className="min-h-screen bg-[#FFF5E1] text-[#1E3D59]">
       <NavBar onSmoothScroll={smoothScroll} activeSection={activeSection} />
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        src="/background.mp4"
+        type="video/mp4"
+        autoPlay
+        loop
+        muted
+      />
+      <div className="absolute top-0 left-0 w-full h-full bg-[#FFF5E1] opacity-90"></div>
 
-      <main className="pt-40 container mx-auto px-4 py-12">
-        <Hero
-          title={eventInfo.title}
-          title2={eventInfo.title2}
-          subTitle={eventInfo.subTitle}
-          logos={eventInfo.logos}
-          onSmoothScroll={smoothScroll}
-        />
-        {markdownContents['Event Information'] && (
-          <div id="information" ref={sectionRefs.information}>
-            <Info content={markdownContents['Event Information']} />
-          </div>
-        )}
-        {scheduleTitle && scheduleItems && (
-          <div id="schedule" ref={sectionRefs.schedule}>
-            <Schedule title={scheduleTitle} scheduleItems={scheduleItems} />
-          </div>
-        )}
-        {metaData.Participants && markdownContents.Participants && (
-          <div id="participants" ref={sectionRefs.participants} className="min-h-screen">
-            <Participants 
-              markdownContent={markdownContents.Participants}
-            />
-          </div>
-        )}
-        {markdownContents.Gallery && metaData.Gallery && (
-          <div id="gallery" ref={sectionRefs.gallery}>
-            <MediaGallery
-              markdown={markdownContents.Gallery}
-              metaData={{
-                ...metaData.Gallery,
-                title: metaData.Gallery.title || "Galeria da Edição Anterior"
-              }}
-            />
-          </div>
-        )}
-      </main>
+      <main className="container mx-auto px-4 py-12">
+        <div className='relative z-10 my-48'>
+          <Hero
+            title={eventInfo.title}
+            title2={eventInfo.title2}
+            subTitle={eventInfo.subTitle}
+            logos={eventInfo.logos}
+            onSmoothScroll={smoothScroll}
+          />
+        </div>
+        <div className="relative w-full h-12 mt-12">
+          <div className="absolute left-0 w-1/2 h-full bg-[#FFF5E1] clip-triangle-left"></div>
+          <div className="absolute right-0 w-1/2 h-full bg-[#FFF5E1] clip-triangle-right"></div>
+        </div>
+
+        {
+          markdownContents['Event Information'] && (
+            <div id="information" className='pt-20' ref={sectionRefs.information}>
+              <Info content={markdownContents['Event Information']} />
+            </div>
+          )
+        }
+        {
+          scheduleTitle && scheduleItems && (
+            <div id="schedule" ref={sectionRefs.schedule}>
+              <Schedule title={scheduleTitle} scheduleItems={scheduleItems} />
+            </div>
+          )
+        }
+        {
+          metaData.Participants && markdownContents.Participants && (
+            <div id="participants" ref={sectionRefs.participants} className="min-h-screen">
+              <Participants
+                markdownContent={markdownContents.Participants}
+              />
+            </div>
+          )
+        }
+        {
+          markdownContents.Gallery && metaData.Gallery && (
+            <div id="gallery" ref={sectionRefs.gallery}>
+              <MediaGallery
+                markdown={markdownContents.Gallery}
+                metaData={{
+                  ...metaData.Gallery,
+                  title: metaData.Gallery.title || "Galeria da Edição Anterior"
+                }}
+              />
+            </div>
+          )
+        }
+      </main >
 
       <Footer />
-    </div>
+    </div >
   );
 };
 
